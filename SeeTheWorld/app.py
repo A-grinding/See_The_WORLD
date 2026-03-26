71c5c79cf8026f5eb88cfd42d2d598ae78864359
A-grinding <abhishekd0074@op.iitg.ac.in>
2026-03-27 02:01:41 +0530

    initial commit

---
+++ SeeTheWorld/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.set_page_config(page_title= "See The WORLD", page_icon=":ringed_planet:", layout="wide")
+
+st.subheader("See The WORLD")
+st.title("The World is made by people and people are made of THOUGHTS")
+st.write("See The WORLD project let's you see the Thoughts of the people worldwide on aparticular topic that you decide. These Thoughts are presented to you in an interactive manner. Thus enabling you to SEE what the WORLD thinks about what you are thinking :wave:")
